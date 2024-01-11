16e49c66cb3f0b39621422e86755ef4c1e4eb2a8
Tim Delaney <delanetw@mail.uc.edu>
2024-01-11 13:56:00 -0500

    initial

---
+++ src/PersonGenerator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+        //Person generator
+        //a.    ID (a String)
+        //b.    FirstName
+        //c.    LastName
+        //d.    Title (a string like Mr., Mrs., Ms., Dr., etc.)
+        //e.    YearOfBirth (an Int)
+
+        String ID = "";
+        String fName = "";
+        String lName = "";
+        String title = "";
+        int YOB = 0;
+        String csvRec = "";
+        boolean done = false;
+
+        Scanner in = new Scanner(System.in);
+    }
+}
